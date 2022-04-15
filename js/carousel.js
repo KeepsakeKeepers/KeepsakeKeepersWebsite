7f48289eebd9516b11fbcb2e2488b55cd2e67b5b
KeepsakeKeepers <103752197+KeepsakeKeepers@users.noreply.github.com>
2022-04-15 15:40:14 +0300

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,85 @@
+function main(){
+  const slider = document.querySelector(".slider");
+  const nextBtn = document.querySelector(".next-btn");
+  const prevBtn = document.querySelector(".prev-btn");
+  const slides = document.querySelectorAll(".slide");
+  const slideIcons = document.querySelectorAll(".slide-icon");
+  const numberOfSlides = slides.length;
+
+  var slideNumber = 0;
+
+  nextBtn.addEventListener("click", () => {
+
+    slides.forEach((slide)=>{
+      slide.classList.remove("active");
+    });
+
+    slideIcons.forEach((slideIcon)=>{
+      slideIcon.classList.remove("active");
+    });
+
+    slideNumber++;
+
+    if( slideNumber > (numberOfSlides - 1)){
+      slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+  });
+
+  prevBtn.addEventListener("click", () => {
+
+    slides.forEach((slide)=>{
+      slide.classList.remove("active");
+    });
+
+    slideIcons.forEach((slideIcon)=>{
+      slideIcon.classList.remove("active");
+    });
+
+    slideNumber--;
+
+    if( slideNumber < 0){
+      slideNumber = numberOfSlides - 1;
+    }
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+  });
+
+  //Image Slider Autoplay
+  var playSlider;
+  
+  var repeater = () =>{
+    playSlider = setInterval(function(){
+      slides.forEach((slide)=>{
+        slide.classList.remove("active");
+      });
+  
+      slideIcons.forEach((slideIcon)=>{
+        slideIcon.classList.remove("active");
+      });
+  
+      slideNumber++;
+  
+      if( slideNumber > (numberOfSlides - 1)){
+        slideNumber = 0;
+      }
+  
+      slides[slideNumber].classList.add("active");
+      slideIcons[slideNumber].classList.add("active");
+    }, 4000)
+  };
+  repeater();
+
+  //Stop the Autoplay in mouseover
+  slider.addEventListener("mouseover", () => {
+    clearInterval(playSlider);
+  });
+
+  //Start the Autoplay in mouseout
+  slider.addEventListener("mouseout",() => {
+    repeater();
+  });
+}
