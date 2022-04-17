0c1fdf0beefbb3acb9678736c30d63cdc78b7e2c
KeepsakeKeepers <103752197+KeepsakeKeepers@users.noreply.github.com>
2022-04-17 14:39:23 +0300

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector(".menu");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", ()=>{
+    const visibility = menu.getAttribute("data-visible");
+
+    if(visibility === "false"){
+        menu.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    }else{
+        menu.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+});
